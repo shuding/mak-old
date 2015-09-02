7b14d7e05f2d1bffabe949cf806e8f302b7b1713
quietshu <ds303077135@gmail.com>
2015-09-02 21:54:40 +0800

    Add README & KaTeX & highlight.js.

--- app/gui.js
+++ app/gui.js
@@ -11,7 +11,10 @@ function mainEditorCtrl($scope, $sce) {
 
     $scope.preview = {
         styleElement: null,
-        themes:       ['default', 'foghorn']
+        codeStyleElement: null,
+        katexStyleElement: null,
+        theme:        'default',
+        codeTheme:    'xcode'
     };
 
     function init() {
@@ -19,17 +22,27 @@ function mainEditorCtrl($scope, $sce) {
         $scope.$watch('editor.context', function (newContext) {
             $scope.editor.compiled = $sce.trustAsHtml(mak.marked(newContext));
         });
-        $scope.editor.context = '# hello, world';
+        $scope.editor.context = '# Mak\n\nA minimal Markdown editor on the web.\n\n---\n\n```js\nconsole.log("hello"); /* code highlighting */\n```\n\nLaTeX expression: $x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}$.\n\n---\n\n[GitHub](https://github.com/quietshu/mak), MIT licensed.\n\n<3\n';
 
         $scope.preview.styleElement      = document.createElement('link');
         $scope.preview.styleElement.rel  = 'stylesheet';
         $scope.preview.styleElement.type = 'text/css';
 
+        $scope.preview.codeStyleElement = document.createElement('link');
+        $scope.preview.codeStyleElement.rel = 'stylesheet';
+        $scope.preview.codeStyleElement.type = 'text/css';
+
+        $scope.preview.katexStyleElement = document.createElement('link');
+        $scope.preview.katexStyleElement.rel = 'stylesheet';
+        $scope.preview.katexStyleElement.type = 'text/css';
+
         loadTheme();
     }
 
-    function loadTheme(theme) {
-        $scope.preview.styleElement.href = './theme/' + (theme || 'default') + '.css';
+    function loadTheme() {
+        $scope.preview.styleElement.href = './theme/' + ($scope.preview.theme || 'default') + '.css';
+        $scope.preview.codeStyleElement.href = '../node_modules/highlight.js/styles/' + ($scope.preview.codeTheme || 'default') + '.css';
+        $scope.preview.katexStyleElement.href = '../bower_components/katex/dist/katex.min.css';
     }
 
     init();
@@ -41,6 +54,8 @@ function previewIframe() {
         link:     function ($scope, element) {
             var document = element[0].contentDocument || element[0].contentWindow.document;
             document.head.appendChild($scope.preview.styleElement);
+            document.head.appendChild($scope.preview.codeStyleElement);
+            document.head.appendChild($scope.preview.katexStyleElement);
 
             $scope.$watch('editor.compiled', function (newCompiled) {
                 document.body.innerHTML = newCompiled;
