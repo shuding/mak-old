6fc7332af7dfa5d5699d753dc86d0c730c0816a4
Shu <ds303077135@gmail.com>
2017-02-28 15:35:17 +0800

    add word count

--- gui.js
+++ gui.js
@@ -38,9 +38,9 @@ function mainEditorCtrl($scope, $sce) {
                 localStorage.setItem('mak', newContext);
             $scope.editor.compiled = $sce.trustAsHtml(mak.marked(newContext));
         });
-        
+
         var context = (localStorage.getItem && localStorage.getItem('mak')) || '# Mak\n\nA minimal Markdown editor on the web.\n\nChanges will be saved into browser\'s localStorage.\n\nEditor only: [/?editor](/mak/?editor). Preview only: [/?preview](/mak/?preview) and then you can save it as a PDF. \n\n---\n\n```js\nconsole.log("hello"); /* code highlighting */\n```\n\nInline LaTeX expression: $x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}$.\n\nMath equation: $$X_{k}=\\sum_{n=0}^{N}x(n)e^{-i2\\pi nk/N}$$\n\n---\n\n[GitHub](https://github.com/quietshu/mak), MIT licensed.\n\n<3\n';
-        
+
         $scope.editor.context = context;
 
         $scope.preview.styleElement      = document.createElement('link');
@@ -75,7 +75,7 @@ function previewIframe() {
             document.head.appendChild($scope.preview.styleElement);
             document.head.appendChild($scope.preview.codeStyleElement);
             document.head.appendChild($scope.preview.katexStyleElement);
-            
+
             var D = null;
             var search = window.location.search
             if (search == '?1' || search == '?preview') {
@@ -88,6 +88,8 @@ function previewIframe() {
             }
 
             $scope.$watch('editor.compiled', function (newCompiled) {
+                var context = localStorage.getItem('mak')
+                newCompiled += `<div class='word-count'>Characters: ${context.length}, words: ${context.split(' ').length}</div>`
                 if (D) {
                     D.innerHTML = newCompiled;
                 } else {
